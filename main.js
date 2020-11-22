7b316df48236ccf2dde1130f91a86e8cef4c9ea6
Hitesha222 <hitesha.v.chauhan@gmil.com>
2020-11-22 14:55:44 +0000

    create api for forecast

--- main.js
+++ main.js
@@ -100,6 +100,10 @@ function searchCity(city) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   console.log(apiUrl);
   axios.get(apiUrl).then(displayWeather);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function onSubmit(event) {
