ba903f21aa6a819784cd9ec0fab308c5bbddac5c
Hitesha222 <hitesha.v.chauhan@gmil.com>
2020-11-24 22:01:12 +0000

    update css file

--- main.js
+++ main.js
@@ -107,7 +107,7 @@ function displayForecast(responce) {
 
     forecastElement.innerHTML += `<div class="col">
 			<div class="card">
-				<div class="card-body text-center" style="height:60px;padding:0;">
+				<div class="card-body text-center" >
           <h5 class="card-title">
                 ${forecastDay(forecast.dt * 1000)}
           </h5>
